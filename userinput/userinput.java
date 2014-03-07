b56a30a341bc520b6034e70bada95c09d5f75739
Henry Harris <contact@dummycode.com>
2014-03-06 21:45:00 -0700

    .

--- userinput/userinput.java
+++ userinput/userinput.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class UserInput {
+public class userinput {
 	public static void main(String args[]) {
 		System.out.print("Input: ");
 		Scanner scan = new Scanner(System.in);
