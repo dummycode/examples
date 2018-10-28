678f8d0dccbe4f2c10c560388be46ae89b697295
Henry Harris <henrythomasharris@gmail.com>
2018-10-28 13:13:04 -0400

    Cannot use Python standards

--- LeetCode/540/simple.py
+++ LeetCode/540/simple.py
@@ -8,7 +8,7 @@ class Solution:
         length = len(nums)
         
         while i < length - 1:
-            if nums[i] != nums[i + 1]:
+            if (nums[i] != nums[i + 1]):
                 return nums[i]
             i += 2
             
